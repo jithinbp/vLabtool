5bfaba0ec708c3437561c97d71752f84e6afbbd2
georgesk <georgesk@debian.org>
2015-12-26 10:34:53 +0100

    fixed insconsistent spaces/tabs

--- vLabtool/tests/tests.py
+++ vLabtool/tests/tests.py
@@ -3,17 +3,17 @@ from __future__ import print_function
 import unittest
 
 class TestUnit(unittest.TestCase):
- 
+
     def setUp(self):
         import Labtools
         print ('import successful')
         pass
- 
+
     def test_voltage_read(self):
-    	import Labtools.interface as i
-    	I=i.Interface()
+        import Labtools.interface as i
+        I=i.Interface()
         print (self.I.get_average_voltage('CH1'))
- 
- 
+
+
 if __name__ == '__main__':
     unittest.main()
